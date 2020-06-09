a90b6c3863a074262c2fbe41aac0c5ae683c8239
Emran Imam <emran.imam@gmail.com>
2020-06-09 17:22:29 +0600

    ios

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,7 +5,6 @@
 // ignore: unused_import
 import 'dart:ui';
 
-import 'package:audioplayers/audioplayers_web.dart';
 import 'package:just_audio/just_audio_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
@@ -13,7 +12,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
-  AudioplayersPlugin.registerWith(registry.registrarFor(AudioplayersPlugin));
   JustAudioPlugin.registerWith(registry.registrarFor(JustAudioPlugin));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
   registry.registerMessageHandler();
